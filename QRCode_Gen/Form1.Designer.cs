231cb0ba60cde8cb006863e54ce323680d5ecea5
Kelin Ronaldo-RKELIN <rkelin@motorola.com>
2023-11-22 12:54:56 -0300

    First commit

---
+++ QRCode_Gen/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace QRCode_Gen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picQRCodeLeft = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtLargura = new System.Windows.Forms.TextBox();
+            this.btnGerarQRCode = new System.Windows.Forms.Button();
+            this.picQRCodeRight = new System.Windows.Forms.PictureBox();
+            this.textInfo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCodeLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCodeRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picQRCodeLeft
+            // 
+            this.picQRCodeLeft.Location = new System.Drawing.Point(12, 12);
+            this.picQRCodeLeft.Name = "picQRCodeLeft";
+            this.picQRCodeLeft.Size = new System.Drawing.Size(200, 200);
+            this.picQRCodeLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picQRCodeLeft.TabIndex = 0;
+            this.picQRCodeLeft.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Informe o produto e estação";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(12, 251);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(210, 20);
+            this.txtTexto.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Largura";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(13, 291);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(66, 20);
+            this.txtAltura.TabIndex = 5;
+            this.txtAltura.Text = "400";
+            // 
+            // txtLargura
+            // 
+            this.txtLargura.Location = new System.Drawing.Point(95, 291);
+            this.txtLargura.Name = "txtLargura";
+            this.txtLargura.Size = new System.Drawing.Size(60, 20);
+            this.txtLargura.TabIndex = 6;
+            this.txtLargura.Text = "400";
+            // 
+            // btnGerarQRCode
+            // 
+            this.btnGerarQRCode.Location = new System.Drawing.Point(351, 274);
+            this.btnGerarQRCode.Name = "btnGerarQRCode";
+            this.btnGerarQRCode.Size = new System.Drawing.Size(95, 34);
+            this.btnGerarQRCode.TabIndex = 7;
+            this.btnGerarQRCode.Text = "QRCode Viwer";
+            this.btnGerarQRCode.UseVisualStyleBackColor = true;
+            this.btnGerarQRCode.Click += new System.EventHandler(this.btnGerarQRCode_Click);
+            // 
+            // picQRCodeRight
+            // 
+            this.picQRCodeRight.Location = new System.Drawing.Point(246, 12);
+            this.picQRCodeRight.Name = "picQRCodeRight";
+            this.picQRCodeRight.Size = new System.Drawing.Size(200, 200);
+            this.picQRCodeRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picQRCodeRight.TabIndex = 8;
+            this.picQRCodeRight.TabStop = false;
+            // 
+            // textInfo
+            // 
+            this.textInfo.Location = new System.Drawing.Point(17, 330);
+            this.textInfo.Multiline = true;
+            this.textInfo.Name = "textInfo";
+            this.textInfo.Size = new System.Drawing.Size(428, 108);
+            this.textInfo.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 516);
+            this.Controls.Add(this.textInfo);
+            this.Controls.Add(this.picQRCodeRight);
+            this.Controls.Add(this.btnGerarQRCode);
+            this.Controls.Add(this.txtLargura);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picQRCodeLeft);
+            this.Name = "Form1";
+            this.Text = "QRCode Gen";
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCodeLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCodeRight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picQRCodeLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtLargura;
+        private System.Windows.Forms.Button btnGerarQRCode;
+        private System.Windows.Forms.PictureBox picQRCodeRight;
+        private System.Windows.Forms.TextBox textInfo;
+    }
+}
+
---
+++ QRCode_Gen/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace QRCode_Gen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Bitmap GerarQRCode(int width, int height, string text)
+        {
+            try
+            {
+                var bw = new ZXing.BarcodeWriter();
+                var encOptions = new ZXing.Common.EncodingOptions() { Width = width, Height = height, Margin = 0 };
+                bw.Options = encOptions;
+                bw.Format = ZXing.BarcodeFormat.QR_CODE;
+                var resultado = new Bitmap(bw.Write(text));
+                return resultado;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private void btnGerarQRCode_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text == string.Empty || txtLargura.Text == string.Empty && txtLargura.Text == string.Empty)
+            {
+                MessageBox.Show("Informações inválidas. Complete as informações para gerar o QRCode.");
+                txtTexto.Focus();
+                return;
+            }
+            try
+            {
+                int largura = Convert.ToInt32(txtLargura.Text);
+                int altura = Convert.ToInt32(txtAltura.Text);
+                string QRcode_left = "MOTO-WIFI:S:" + txtTexto.Text + ";T:WPA;P:12345678;H:false;IP:192.168.137.10;";
+                string QRcode_right = "MOTO-WIFI:S:" + txtTexto.Text + ";T:WPA;P:12345678;H:false;IP:192.168.137.20;";
+                picQRCodeLeft.Image = GerarQRCode(largura, altura, QRcode_left);
+                picQRCodeRight.Image = GerarQRCode(largura, altura, QRcode_right);
+                textInfo.Text = "SSID:" + txtTexto.Text + "\r\n" + "Senha:12345678";
+                picQRCodeLeft.Image.Save(@"C:\prod\config\QRcode_left.jpeg", ImageFormat.Jpeg);
+                picQRCodeRight.Image.Save(@"C:\prod\config\QRcode_right.jpeg", ImageFormat.Jpeg);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
